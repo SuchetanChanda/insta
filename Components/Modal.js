307efa4302fa459c33562f4b9343b0b92889b51d
SuchetanChanda <92180546+SuchetanChanda@users.noreply.github.com>
2022-04-17 15:29:37 +0530

    Update Modal.js
    Changing the modal styling in for xs screen

--- Components/Modal.js
+++ Components/Modal.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { Fragment, useRef, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
@@ -97,7 +98,7 @@ function Modal() {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 transition-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
+            <div className="mt-20 inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
               <div>
                     {selectedFile ? (
                         <img src={selectedFile} 
