7ec2d6e06dabdf3af625a127a6214fcb13017210
SuchetanChanda <92180546+SuchetanChanda@users.noreply.github.com>
2022-04-17 22:50:26 +0530

    Update Modal.js

--- Components/Modal.js
+++ Components/Modal.js
@@ -144,7 +144,8 @@ function Modal() {
                     </div>
                 </div>
                 <div className="mt-5 sm:mt-6">
-                  <button
+                  {selectedFile ? (
+                    <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none 
                                 focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 hover:disabled:bg-gray-300 sm:text-sm
@@ -153,6 +154,18 @@ function Modal() {
                   >
                       {loading ? "Uploading..." : "Upload Post"}
                   </button>
+                  ): (
+                    <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none 
+                                focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 hover:disabled:bg-gray-300 sm:text-sm
+                                "
+                  >
+                      {loading ? "Uploading..." : "Upload Post"}
+                  </button>
+
+                  )}
+                  
                 </div>
               </div>
             </div>
