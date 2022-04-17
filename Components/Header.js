74dd9a6ff3f5079551c2c0db960220bd65bf482f
SuchetanChanda <92180546+SuchetanChanda@users.noreply.github.com>
2022-04-18 02:31:31 +0530

    Update Header.js
    hidden search input field in small screens

--- Components/Header.js
+++ Components/Header.js
@@ -41,7 +41,7 @@ function Header(){
             />
         </div>
         {/* MIDDLE - Search input field */}
-        <div className=' hidden sm:block sm:max-w-xs'>
+        <div className='hidden sm:block sm:max-w-xs'>
             <div className='relative mt-1 p-3 rounded-md'>
                 <div className=' absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
                     <SearchIcon className='h-5 w-5 sm:h-5 sm:w-5 text-gray-500 '/>
@@ -54,7 +54,7 @@ function Header(){
         {/* RIGHT */}
         <div className='flex items-center justify-end space-x-4'>
         <HomeIcon onClick={()=> router.push("/")} className='navBtn'/>
-        <PlusCircleIcon onClick={() => setOpen(true)} className='h-6 md:hidden cursor-pointer '/>
+        {/* <MenuIcon className='h-6 md:hidden cursor-pointer '/> */}
 
         {session ? (
             <>
@@ -62,7 +62,9 @@ function Header(){
             <PaperAirplaneIcon className='navBtn rotate-45' />
             <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
             </div>
-            <PlusCircleIcon onClick={() => setOpen(true)} className='navBtn' />
+            <PlusCircleIcon onClick={() => setOpen(true)} className='sm:block h-6 md:inline-flex
+            cursor-pointer hover:scale-125
+            transition-all duration-150 ease-out' />
             <UserGroupIcon className='navBtn' />
             <HeartIcon className='navBtn' />
     
@@ -70,7 +72,7 @@ function Header(){
             </>
             
         ): (
-            <button onClick={signIn}>Sign In</button>
+            <button className='w-20' onClick={signIn}>Sign In</button>
         )}
         
         </div>
